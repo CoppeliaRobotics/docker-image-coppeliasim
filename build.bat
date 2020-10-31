c75ede946db0ff478396f1017e919ed6bac19e80
Federico Ferri <federico.ferri.it@gmail.com>
2020-10-31 12:21:40 +0100

    add batch files for Windows

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+set COPPELIASIM_RELEASE=CoppeliaSim_Edu_V4_1_0_Ubuntu18_04.tar.xz
+if not exist download/%COPPELIASIM_RELEASE% (
+    echo Download https://www.coppeliarobotics.com/files/%COPPELIASIM_RELEASE% and place into the download directory located in this repository.
+    exit /b
+)
+echo on
+docker build --rm -f Dockerfile -t coppeliasim-ubuntu18:latest .
---
+++ run.bat
@@ -0,0 +1,1 @@
+docker run -v %CD%\shared:/shared -p 19997:19997 -it coppeliasim-ubuntu18 %*
