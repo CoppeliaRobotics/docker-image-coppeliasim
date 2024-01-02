b251472b8393a01998088b45e30e0a33e989456e
Coppelia <mfreese@gmx.ch>
2024-01-02 16:32:07 +0100

    updated copyright

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
 @echo off
-set COPPELIASIM_RELEASE=CoppeliaSim_Edu_V4_6_0_rev0_Ubuntu22_04.tar.xz
-if not exist download/%COPPELIASIM_RELEASE% (
-    echo Download https://www.coppeliarobotics.com/files/%COPPELIASIM_RELEASE% and place into the download directory located in this repository.
+set COPPELIASIM_RELEASE=V4_6_0_rev16
+set COPPELIASIM_FILE=CoppeliaSim_Edu_%COPPELIASIM_RELEASE%_Ubuntu22_04.tar.xz
+if not exist download/%COPPELIASIM_FILE% (
+    echo Download https://downloads.coppeliarobotics.com/%COPPELIASIM_RELEASE%/%COPPELIASIM_FILE% and place into the download directory located in this repository.
     exit /b
 )
 echo on
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-COPPELIASIM_RELEASE=CoppeliaSim_Edu_V4_6_0_rev0_Ubuntu22_04.tar.xz
-if [ ! -f download/$COPPELIASIM_RELEASE ]; then
+COPPELIASIM_RELEASE="V4_6_0_rev16"
+COPPELIASIM_FILE="CoppeliaSim_Edu_${COPPELIASIM_RELEASE}_Ubuntu22_04.tar.xz"
+if [ ! -f "download/${COPPELIASIM_FILE}" ]; then
     if [ "$1" = "-d" ]; then
         if ! command -v curl > /dev/null 2>&1; then
             echo "Command 'curl' not available" 1>&2
@@ -7,14 +8,14 @@ if [ ! -f download/$COPPELIASIM_RELEASE ]; then
         fi
         rm -rf download/tmp
         mkdir download/tmp
-        echo "Downloading $COPPELIASIM_RELEASE"
+        echo "Downloading ${COPPELIASIM_FILE}"
         cd download/tmp
         curl --progress-bar --remote-name --location \
-            https://www.coppeliarobotics.com/files/$COPPELIASIM_RELEASE || exit 1
-        mv $COPPELIASIM_RELEASE ..
+            https://downloads.coppeliarobotics.com/${COPPELIASIM_RELEASE}/${COPPELIASIM_FILE} || exit 1
+        mv ${COPPELIASIM_FILE} ..
         cd -
     else
-        echo "File 'download/$COPPELIASIM_RELEASE' not found."
+        echo "File 'download/${COPPELIASIM_FILE}' not found."
         echo "Either download it manually, or pass the -d option."
         exit 1
     fi
